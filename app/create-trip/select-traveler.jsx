583e5bf17e9a91493ef3707250a0804b171d1569
Sergiy Antonyuk <s.p.antonyuk@gmail.com>
2024-08-31 19:10:11 +0200

    update

--- app/create-trip/select-traveler.jsx
+++ app/create-trip/select-traveler.jsx
@@ -1,65 +1,75 @@
 import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
-import { Link, useNavigation } from 'expo-router'
-import {Colors } from './../../constants/Colors'
-import {SelectTravelesList} from './../../constants/Options'
+import { useNavigation, useRouter } from 'expo-router'
+import { Colors } from './../../constants/Colors'
+import { SelectTravelesList } from './../../constants/Options'
 import OptionCard from '../../components/CreateTrip/OptionCard'
 import { CreateTripContext } from '../../context/CreateTripContext'
+
 export default function SelectTraveler() {
- 
-  const navigation=useNavigation();
-  const [selectedTraveler,setSelectedTraveler]=useState();
-  const {tripData,setTripData}=useContext(CreateTripContext);
+
+  const navigation = useNavigation();
+  const [selectedTraveler, setSelectedTraveler] = useState();
+  const { tripData, setTripData } = useContext(CreateTripContext);
+  const router = useRouter();
 
 
-  useEffect(()=>{
+  useEffect(() => {
     navigation.setOptions({
-      headerShown:true,
-      headerTransparent:true,
-      headerTitle:''
+      headerShown: true,
+      headerTransparent: true,
+      // headerTitle: ''
     })
-  },[])
+  }, [])
 
-  useEffect(()=>{
-    setTripData({...tripData,
-      traveler:selectedTraveler
+  useEffect(() => {
+    setTripData({
+      ...tripData,
+      traveler: selectedTraveler
     })
-  },[selectedTraveler]);
+  }, [selectedTraveler]);
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(tripData);
-  },[tripData])
+  }, [tripData])
+
+
+  // const onSelectTravelerContinue = () => {
+  //   console.log('tripData: ', tripData);
+  //   router.push('/create-trip/select-date')
+  // }
+
   return (
     <View style={{
-      padding:25,
-      paddingTop:75,
-      backgroundColor:Colors.WHITE,
-      height:'100%'
+      padding: 25,
+      paddingTop: 75,
+      backgroundColor: Colors.WHITE,
+      height: '100%'
     }}>
       <Text style={{
-        fontSize:35,
-        fontFamily:'outfit-bold',
-        marginTop:20
+        fontSize: 35,
+        fontFamily: 'outfit-bold',
+        marginTop: 20
       }}>Who's Traveling </Text>
 
       <View style={{
 
-        marginTop:20
+        marginTop: 20
       }}>
         <Text style={{
-          fontFamily:'outfit-bold',
-          fontSize:23
+          fontFamily: 'outfit-bold',
+          fontSize: 23
         }}>Choose your traveles</Text>
 
         <FlatList
           data={SelectTravelesList}
-          renderItem={({item,index})=>(
-            <TouchableOpacity 
-            onPress={()=>setSelectedTraveler(item)}
-            style={{
-              marginVertical:10
-            }}>
-              
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              onPress={() => setSelectedTraveler(item)}
+              style={{
+                marginVertical: 10
+              }}>
+
               <OptionCard option={item} selectedOption={selectedTraveler} />
             </TouchableOpacity>
           )}
@@ -67,29 +77,27 @@ export default function SelectTraveler() {
 
       </View>
 
-      <TouchableOpacity 
-      style={{
-        padding:15,
-        backgroundColor:Colors.PRIMARY,
-        borderRadius:15,
-        marginTop:20
-      }}>
-
-      <Link href={'/create-trip/select-dates'}
-      style={{
-        width:'100%',
-        textAlign:'center'
-      }}
+      <TouchableOpacity
+        onPress={() => router.push('/create-trip/select-date')}
+        // onPress={onSelectTravelerContinue}
+        style={{
+          padding: 15,
+          backgroundColor: Colors.PRIMARY,
+          borderRadius: 15,
+          marginTop: 20,
+          width: '100%',
+          textAlign: 'center',
+        }}
       >
+
         <Text style={{
-          textAlign:'center',
-          color:Colors.WHITE,
-          fontFamily:'outfit-medium',
-          fontSize:20
+          textAlign: 'center',
+          color: Colors.WHITE,
+          fontFamily: 'outfit-medium',
+          fontSize: 20
         }}>Continue</Text>
-        </Link>
       </TouchableOpacity>
-      
-    </View>
+
+    </View >
   )
 }
